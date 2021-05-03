4d6c0eaacf383c6cd0451f8d2d16623d22d384c6
vasco <vastur@gmail.com>
2021-05-03 02:43:27 +0100

    Upgrade rollup setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,55 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonJs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
-import { name, homepage, version } from './package.json';
+import resolve from '@rollup/plugin-node-resolve';
+import commonJs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import { name, homepage, version, dependencies } from './package.json';
 
-export default {
+const umdConf = {
+  format: 'umd',
+  extend: true,
+  name: 'd3',
+  banner: `// Version ${version} ${name} - ${homepage}`
+};
+
+export default [
+  { // UMD
     input: 'src/index.js',
     output: [
-        {
-            format: 'umd',
-            extend: true,
-            name: 'd3',
-            file: `dist/${name}.js`,
-            sourcemap: true,
-            banner: `// Version ${version} ${name} - ${homepage}`
-        }
+      {
+        ...umdConf,
+        file: `dist/${name}.js`,
+        sourcemap: true
+      },
+      { // minify
+        ...umdConf,
+        file: `dist/${name}.min.js`,
+        plugins: [terser({
+          output: { comments: '/Version/' }
+        })]
+      }
     ],
     plugins: [
-        resolve(),
-        commonJs(),
-        babel({ exclude: 'node_modules/**' })
+      resolve(),
+      commonJs(),
+      babel({ exclude: 'node_modules/**' })
     ]
-};
+  },
+  { // commonJs and ES modules
+    input: 'src/constant-force.js',
+    output: [
+      {
+        format: 'cjs',
+        file: `dist/${name}.common.js`,
+        exports: 'auto'
+      },
+      {
+        format: 'es',
+        file: `dist/${name}.module.js`
+      }
+    ],
+    external: Object.keys(dependencies),
+    plugins: [
+      babel()
+    ]
+  },
+];
